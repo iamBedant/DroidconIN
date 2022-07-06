ff6fed4a685e579f13ac86e24b8b853f765e1eeb
iamBedant <bebi2527@gmail.com>
2022-07-06 21:50:05 +0530

    Adds viewmodel to iOS app

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,11 +1,43 @@
 import SwiftUI
 import shared
+import Combine
+
+
+
+private let log = koin.loggerWithTag(tag: "ViewController")
+
+
+class ObservableScheduleModel : ObservableObject {
+    private var viewModel : ScheduleCallbackViewModel?
+    
+    @Published
+    var loading = false
+    
+    func activate(){
+        let viewModel = KotlinDependencies.shared.getScheduleViewModel()
+        self.viewModel = viewModel
+    }
+    
+    func refresh() {
+        viewModel?.refreshSchedule()
+    }
+    
+}
+
+
+
+
 
 struct ContentView: View {
 	let greet = Greeting().greeting()
-
+    @ObservedObject
+    var observableModel = ObservableScheduleModel()
 	var body: some View {
 		Text(greet)
+            .onAppear(perform: {
+                observableModel.activate()
+                observableModel.refresh()
+            })
 	}
 }
 
